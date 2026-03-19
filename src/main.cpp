2372c9e83ba7e9021d8bc9ee9321f8802941c831
Arnav Goyal <rockinarnav@hotmail.com>
2026-03-19 11:38:22 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <iterator>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <ostream>
@@ -13,6 +14,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
+
+void describeTopicPartitions(const char *buffer, int client_fd);
+void apiVersions(const char *buffer, int client_fd);
 
 int main(int argc, char *argv[]) {
   // Disable output buffering
@@ -100,87 +104,26 @@ int main(int argc, char *argv[]) {
     }
 
     while (r > 0) {
+      int p = 0;
       std::uint32_t size;
-      std::uint32_t correlation_id;
-      std::uint16_t api_ver;
+      std::memcpy(&size, buffer, sizeof(size));
+      size = ntohl(size);
+
+      p += sizeof(size);
       std::uint16_t api_key;
-      std::uint16_t error_code;
-      std::string temp{buffer};
-      std::cout << "String: " << temp << "\n";
-      struct timeval tv;
-
-      std::memcpy(&api_key, buffer + 4, sizeof(std::uint16_t));
+      std::memcpy(&api_key, buffer + p, sizeof(api_key));
       api_key = ntohs(api_key);
-      std::memcpy(&api_ver, buffer + 6, sizeof(std::uint16_t));
-      api_ver = ntohs(api_ver);
 
-      std::memcpy(&correlation_id, buffer + 8, sizeof(std::uint32_t));
-      size = sizeof(correlation_id);
-
-      if (api_ver < 0 || api_ver > 4) {
-        error_code = htons(35);
-      } else {
-        error_code = htons(3);
+      if (api_key == 75) {
+        describeTopicPartitions(buffer, client_fd);
+      } else if (api_key == 18) {
+        apiVersions(buffer, client_fd);
       }
-
-      std::uint8_t topics_array = static_cast<std::uint8_t>(1);
-      // std::uint16_t key = htons(18);
-      // std::uint16_t min = htons(0);
-      // std::uint16_t max = htons(4);
-      // std::uint8_t api_tag = 0x00;
-      // std::uint16_t key2 = htons(75);
-      // std::uint16_t min2 = htons(0);
-      // std::uint16_t max2 = htons(0);
-      std::uint32_t throttle_time = htonl(0);
-      std::uint8_t tag = 0x00;
-      std::uint8_t is_internal = 0x00;
-      std::uint8_t partitions_array = static_cast<std::uint8_t>(0);
-      std::uint32_t topic_authorized_operations = htonl(0);
-      std::uint8_t cursor = static_cast<std::uint8_t>(-1);
-      std::string topic_id = "00000000000000000000000000000000";
-      int start = 52;
-      int count = 0;
-      while (temp.substr(start, 2) != "00") {
-        count += 2;
-        start += 2;
-      }
-      std::string topic_name = temp.substr(52, count);
-      std::uint8_t name_length = topic_name.length() / 2;
-
-      size += sizeof(tag) + sizeof(throttle_time) + sizeof(topics_array) +
-              sizeof(error_code) + sizeof(name_length) +
-              sizeof(topic_name) / 2 + sizeof(topic_id) / 2 +
-              sizeof(is_internal) + sizeof(partitions_array) +
-              sizeof(topic_authorized_operations) + sizeof(tag) +
-              sizeof(cursor) + sizeof(tag);
-      size = htonl(size);
-
-      send(client_fd, &size, sizeof(size), 0);
-      send(client_fd, &correlation_id, sizeof(correlation_id), 0);
-      send(client_fd, &tag, sizeof(tag), 0);
-      send(client_fd, &throttle_time, sizeof(throttle_time), 0);
-      send(client_fd, &topics_array, sizeof(topics_array), 0);
-      send(client_fd, &error_code, sizeof(error_code), 0);
-      send(client_fd, &name_length, sizeof(name_length), 0);
-      send(client_fd, topic_name.c_str(), sizeof(topic_name), 0);
-      send(client_fd, topic_id.c_str(), sizeof(topic_id), 0);
-      send(client_fd, &is_internal, sizeof(is_internal), 0);
-      send(client_fd, &partitions_array, sizeof(partitions_array), 0);
-      send(client_fd, &topic_authorized_operations,
-           sizeof(topic_authorized_operations), 0);
-      // send(client_fd, &api_tag, sizeof(api_tag), 0);
-      // send(client_fd, &key2, sizeof(key2), 0);
-      // send(client_fd, &min2, sizeof(min2), 0);
-      // send(client_fd, &max2, sizeof(max2), 0);
-      // send(client_fd, &api_tag, sizeof(api_tag), 0);
-      // send(client_fd, &throttle_time, sizeof(throttle_time), 0);
-      send(client_fd, &tag, sizeof(tag), 0);
-      send(client_fd, &cursor, sizeof(cursor), 0);
-      send(client_fd, &tag, sizeof(tag), 0);
 
       bzero(buffer, 256);
       r = 0;
 
+      struct timeval tv;
       tv.tv_sec = 1;
       tv.tv_usec = 0;
       int activity = select(maxfd + 1, &masterfds, NULL, NULL, &tv);
@@ -199,4 +142,142 @@ int main(int argc, char *argv[]) {
 
   close(server_fd);
   return 0;
+}
+
+void describeTopicPartitions(const char *buffer, int client_fd) {
+  int p = 0;
+  std::uint32_t size;
+  std::memcpy(&size, buffer, sizeof(size));
+  size = ntohl(size);
+
+  p += sizeof(size);
+  std::uint16_t api_key;
+  std::memcpy(&api_key, buffer + p, sizeof(api_key));
+  api_key = ntohs(api_key);
+  p += sizeof(api_key);
+  std::uint16_t api_ver;
+  std::memcpy(&api_ver, buffer + p, sizeof(api_ver));
+  api_ver = ntohs(api_ver);
+
+  p += sizeof(api_ver);
+  std::uint32_t correlation_id;
+  std::memcpy(&correlation_id, buffer + p, sizeof(correlation_id));
+
+  p += sizeof(correlation_id);
+  std::uint16_t length;
+  std::memcpy(&length, buffer + p, sizeof(length));
+  length = ntohs(length);
+  std::cout << "LENGTH " << length << "\n";
+
+  p += sizeof(length) + (int)length + 1;
+  std::uint8_t array_length;
+  std::memcpy(&array_length, buffer + p, sizeof(array_length));
+  std::cout << "ARRAY LENGTH " << (int)array_length << "\n";
+
+  p += sizeof(array_length);
+  std::uint8_t name_length;
+  std::memcpy(&name_length, buffer + p, sizeof(name_length));
+  std::cout << "NAME LENGTH " << (int)name_length << "\n";
+
+  p += 1;
+  std::vector<std::uint8_t> topic_name;
+  topic_name.insert(topic_name.end(), &buffer[p],
+                    &buffer[p + (int)name_length - 1]);
+  // p + (int)name_length - 1
+
+  std::uint32_t throttle_time = htonl(0);
+  std::uint8_t tag = 0x00;
+  std::uint8_t is_internal = 0x00;
+  std::uint8_t partitions_array = static_cast<std::uint8_t>(0);
+  std::uint32_t topic_authorized_operations = htonl(0);
+  std::uint8_t cursor = static_cast<std::uint8_t>(-1);
+  std::vector<std::uint8_t> topic_id;
+  std::uint8_t zeroes = static_cast<std::uint8_t>(0);
+  topic_id.insert(topic_id.end(), 16, zeroes);
+
+  std::cout << "TOPIC ID " << topic_id.size() << "\n";
+  std::uint16_t error_code = htons(3);
+  size = sizeof(correlation_id) + sizeof(tag) + sizeof(throttle_time) +
+         sizeof(array_length) + sizeof(error_code) + sizeof(name_length) +
+         topic_name.size() + topic_id.size() + sizeof(is_internal) +
+         sizeof(partitions_array) + sizeof(topic_authorized_operations) +
+         sizeof(tag) + sizeof(cursor) + sizeof(tag);
+  size = htonl(size);
+
+  send(client_fd, &size, sizeof(size), 0);
+  send(client_fd, &correlation_id, sizeof(correlation_id), 0);
+  send(client_fd, &tag, sizeof(tag), 0);
+  send(client_fd, &throttle_time, sizeof(throttle_time), 0);
+  send(client_fd, &array_length, sizeof(array_length), 0);
+  send(client_fd, &error_code, sizeof(error_code), 0);
+  send(client_fd, &name_length, sizeof(name_length), 0);
+  send(client_fd, topic_name.data(), topic_name.size(), 0);
+  send(client_fd, topic_id.data(), topic_id.size(), 0);
+  send(client_fd, &is_internal, sizeof(is_internal), 0);
+  send(client_fd, &partitions_array, sizeof(partitions_array), 0);
+  send(client_fd, &topic_authorized_operations,
+       sizeof(topic_authorized_operations), 0);
+  send(client_fd, &tag, sizeof(tag), 0);
+  send(client_fd, &cursor, sizeof(cursor), 0);
+  send(client_fd, &tag, sizeof(tag), 0);
+}
+
+void apiVersions(const char *buffer, int client_fd) {
+  int p = 0;
+  std::uint32_t size;
+  std::memcpy(&size, buffer, sizeof(size));
+  size = ntohl(size);
+
+  p += sizeof(size);
+  std::uint16_t api_key;
+  std::memcpy(&api_key, buffer + p, sizeof(api_key));
+  api_key = ntohs(api_key);
+
+  p += sizeof(api_key);
+  std::uint16_t api_ver;
+  std::memcpy(&api_ver, buffer + p, sizeof(api_ver));
+  api_ver = ntohs(api_ver);
+
+  p += sizeof(api_ver);
+  std::uint32_t correlation_id;
+  std::memcpy(&correlation_id, buffer + p, sizeof(correlation_id));
+
+  std::uint16_t error_code;
+  if (api_ver < 0 || api_ver > 4) {
+    error_code = htons(35);
+  } else {
+    error_code = htons(0);
+  }
+
+  std::uint8_t length = 0x03;
+  std::uint16_t key = htons(18);
+  std::uint16_t min = htons(0);
+  std::uint16_t max = htons(4);
+  std::uint8_t api_tag = 0x00;
+  std::uint16_t key2 = htons(75);
+  std::uint16_t min2 = htons(0);
+  std::uint16_t max2 = htons(0);
+  std::uint32_t throttle_time = htonl(0);
+  std::uint8_t tag = 0x00;
+
+  size = sizeof(correlation_id) + sizeof(error_code) + sizeof(length) +
+         sizeof(key) + sizeof(min) + sizeof(max) + sizeof(api_tag) +
+         sizeof(key) + sizeof(min) + sizeof(max) + sizeof(api_tag) +
+         sizeof(throttle_time) + sizeof(tag);
+  size = htonl(size);
+
+  send(client_fd, &size, sizeof(size), 0);
+  send(client_fd, &correlation_id, sizeof(correlation_id), 0);
+  send(client_fd, &error_code, sizeof(error_code), 0);
+  send(client_fd, &length, sizeof(length), 0);
+  send(client_fd, &key, sizeof(key), 0);
+  send(client_fd, &min, sizeof(min), 0);
+  send(client_fd, &max, sizeof(max), 0);
+  send(client_fd, &api_tag, sizeof(api_tag), 0);
+  send(client_fd, &key2, sizeof(key2), 0);
+  send(client_fd, &min2, sizeof(min2), 0);
+  send(client_fd, &max2, sizeof(max2), 0);
+  send(client_fd, &api_tag, sizeof(api_tag), 0);
+  send(client_fd, &throttle_time, sizeof(throttle_time), 0);
+  send(client_fd, &tag, sizeof(tag), 0);
 }
